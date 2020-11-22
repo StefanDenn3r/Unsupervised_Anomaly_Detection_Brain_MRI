Minor bug fixes. - Fixed bottleneck name for constrained AE - Fixed bug in calculation for FPR as pointed out in Issue #3 (https://github.com/StefanDenn3r/Unsupervised_Anomaly_Detection_Brain_MRI/issues/3)

models/constrained_autoencoder.py
@@ -26,8 +26,8 @@ def constrained_autoencoder(x, dropout_rate, dropout, config):
         z_layer = Dense(config.zDim)
         dec_dense = Dense(np.prod(reshape))
 
-        outputs['z_'] = z_ = dropout_layer(z_layer(Flatten()(temp_out)), dropout)
-        reshaped = tf.reshape(dropout_layer(dec_dense(z_), dropout), [-1, *reshape])
+        outputs['z'] = z = dropout_layer(z_layer(Flatten()(temp_out)), dropout)
+        reshaped = tf.reshape(dropout_layer(dec_dense(z), dropout), [-1, *reshape])
         temp_out = intermediate_conv_reverse(reshaped)
 
     with tf.variable_scope('Decoder'):

trainers/ConstrainedAE.py
@@ -19,7 +19,7 @@ class ConstrainedAE(AEMODEL):
         self.x = tf.placeholder(tf.float32, [None, self.config.outputHeight, self.config.outputWidth, self.config.numChannels], name='x')
         self.outputs = self.network(self.x, dropout_rate=self.dropout_rate, dropout=self.dropout, config=self.config)
         self.reconstruction = self.outputs['x_hat']
-        self.z_ = self.outputs['z_']
+        self.z = self.outputs['z']
         self.z_rec = self.outputs['z_rec']
 
         self.rho = self.config.rho

trainers/Metrics.py
@@ -87,9 +87,9 @@ def tpr(P, G):
 
 
 def fpr(P, G):
-    tp = np.sum(np.multiply(P.flatten(), G.flatten()))
+    tn = np.sum(np.multiply(np.invert(P.flatten()), np.invert(G.flatten())))
     fp = np.sum(np.multiply(P.flatten(), np.invert(G.flatten())))
-    return fp / (fp + tp)
+    return fp / (fp + tn)
 
 
 def precision(P, G):